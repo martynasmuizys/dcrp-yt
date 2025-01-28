03c88753720d9f8c452a11900a1df04ce327be66
Martynas Muizys <muizysmartynas@gmail.com>
2025-01-28 15:51:51 +0200

    smth

--- src/main.cpp
+++ src/main.cpp
@@ -3,22 +3,37 @@
 #include <vector>
 #include <fstream>
 
+#include "discord.h"
+#include "core.h"
+
+struct DiscordState {
+    discord::User currentUser;
+
+    std::unique_ptr<discord::Core> core;
+};
+
 int main() {
     unsigned int recv_msg_len = 0;
-
     std::cin.read((char*)&recv_msg_len, 4);
     std::vector<char> buf(recv_msg_len);
+
     char* data = new char[recv_msg_len];
     std::cin.read(data, recv_msg_len);
-    
-    // std::string message(buf.begin(), buf.end());
 
-    // std::string msg = "pong";
-    // msg = message;
+    DiscordState state{};
 
-    // unsigned int msg_len = strlen(msg.c_str());
-    std::cout.write((char*)&recv_msg_len , 4);
-    std::cout << data << std::flush;
+    // discord::Core* core{};
+    // auto result = discord::Core::Create(1333609267143643278, DiscordCreateFlags_Default, &core);
+    // state.core.reset(core);
+    // if (!state.core) {
+    //     std::cout << "Failed to instantiate discord core! (err " << static_cast<int>(result)
+    //               << ")\n";
+    //     std::exit(-1);
+    // }
+
+    // unsigned int msg_len = strlen(state.currentUser.GetUsername());
+    // std::cout.write((char*)&recv_msg_len , 4);
+    // std::cout << state.currentUser.GetUsername() << std::flush;
 
     delete[] data;
 
